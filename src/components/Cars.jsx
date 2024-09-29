683fad1d5986085835f10511eefcfe3c631edd0f
Omer <farukoncu78@gmail.com>
2024-09-29 22:48:41 +0300

    up

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -9,13 +9,15 @@ const Cars = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Cars
       </h3>
-      <Row>
+      <Row className="">
         {carData.map(({id, name, type, img}) => (
           <Col xs={6} sm={4} lg={3} key={id}>
-            <Image src={img} className="img-thumbnail car-img " />
+            <Image src={img} className="img-thumbnail car-img" />
+            <p>{name}</p>
           </Col>
         ))}
       </Row>
+      
     </Container>
   );
 };
