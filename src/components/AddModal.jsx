fd6de4d9cca6590707ba97828f95ab29d7c33f63
Omer <farukoncu78@gmail.com>
2024-09-29 23:43:56 +0300

    up

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,32 +1,69 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
-function AddModal() {
-  const [show, setShow] = useState(false);
+function AddModal({ show, setShow, car }) {
+
+  const [customerName, setCustomerName] = useState('')
+  const [date, setDate] = useState('')
+  //const [show, setShow] = useState(false);
+
+  // const addCar = (new) => {
+
+
+  // }
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const handleShow = () => setShow(true);
+  console.log(date)
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+
+  }
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Reservation for {car} </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Customer Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                id="name"
+                value={customerName}
+                onChange={(e)=>setCustomerName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Reservation Date:</Form.Label>
+              <Form.Control 
+              type="datetime-local" 
+              id="date"
+              value={date}
+              onChange={(e)=>setDate(e.target.value)}
+              />
+            </Form.Group>
+
+            <div className="text-center">
+              <Button className="me-2" variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="success" type="submit" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { carData } from "../helpers/data";
+import AddModal from "./AddModal";
 
 const Cars = () => {
   console.log(carData);
+
+  const [show, setShow] = useState(false);
+  const [car, setCar] = useState('')
+
   return (
     <Container>
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Cars
       </h3>
       <Row className="">
-        {carData.map(({id, name, type, img}) => (
+        {carData.map(({ id, name, type, img }) => (
           <Col xs={6} sm={4} lg={3} key={id}>
-            <Image src={img} className="img-thumbnail car-img" />
+            <Image
+              src={img}
+              className="img-thumbnail car-img"
+              onClick={() => {
+                setShow(true)
+                setCar(name)}}
+            />
             <p>{name}</p>
           </Col>
         ))}
       </Row>
-      
+
+      <AddModal show={show} setShow={setShow} car={car} />
     </Container>
   );
 };
